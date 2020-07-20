84d938b30310db4a37ad837a256de7db4bbea81e
Hafidh Pradipta P <hamstergeek@gmail.com>
2020-07-20 20:54:13 +0700

    setup

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const authToken = '5b1b442446754fbb8547ca7860b68860'
+// isi auth token api football
+const authToken = ''
 export const baseurl = 'https://api.football-data.org/v2/'
 export const standing = `${baseurl}competitions/2021/standings`
 
